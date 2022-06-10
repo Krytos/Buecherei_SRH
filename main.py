223359a43557cf53947ea3282e28c5d6638c4b7a
mnitt <rolf.nibotski@gmail.com>
2022-06-10 08:37:51 +0200

    test

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from requests import get
+from bs4 import BeautifulSoup
 
-r = get("https://www.goodreads.com/choiceawards/best-fantasy-books-2021")
-print(r.status_code)
+URL = "https://www.goodreads.com"
+r = get(URL)
+
+soup = BeautifulSoup(r.content, "html.parser")
+result = soup.find_all("a", class_="gr-hyperlink")
+
+link1 = []
+link2 = []
+
+for best_books in result:
+    spl = best_books["href"].split("/")
+    if spl[1] == "genres":
+        URL2 = URL + str(best_books["href"])
+        print("URL2: " + URL2)
+        r2 = get(URL2)
+        soup2 = BeautifulSoup(r2.content, "html.parser")
+        divs = soup2.find_all("div", class_="coverWrapper")
+        for books in divs:
+            URL3 = URL + str(books.find("a")["href"])
+            print("URL3: " + URL3)
+            link2.append(URL3)
+        #print(link2)
